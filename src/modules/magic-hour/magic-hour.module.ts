d342b3f4887fe5d03770757865406a7c53d33583
DestinPQ Support <support@destinpq.com>
2025-06-27 11:04:11 +0530

    fix: Add missing S3UploadModule import to MagicHourModule
    - Fixed dependency injection error where S3UploadService was not available
    - Added S3UploadModule to imports in MagicHourModule
    - Resolves startup error: 'Nest can't resolve dependencies of the MagicHourService'
    - Now MagicHourService can properly inject S3UploadService

--- src/modules/magic-hour/magic-hour.module.ts
+++ src/modules/magic-hour/magic-hour.module.ts
@@ -3,9 +3,10 @@ import { HttpModule } from '@nestjs/axios';
 import { MagicHourController } from './magic-hour.controller';
 import { MagicHourService } from './magic-hour.service';
 import { DatabaseModule } from '../../database/database.module';
+import { S3UploadModule } from '../s3-upload/s3-upload.module';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [DatabaseModule, HttpModule, S3UploadModule],
   controllers: [MagicHourController],
   providers: [MagicHourService],
   exports: [MagicHourService],
